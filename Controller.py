46324e6672f1114af602f68a1a6a09911e4107bf
SteelPotathor <97548610+SteelPotathor@users.noreply.github.com>
2023-01-18 15:45:42 +0100

    Create Controller.py

---
+++ Controller.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+GESTION DE L'INTERFACE UTILISATEUR
+"""
+
+
+# Import du module nécessaire au programme
+from View import *
+
+# On éxécute la fonction qui lance le launcher
+affichage()
